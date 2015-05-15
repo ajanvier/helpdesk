bd4e0935fe73c098fa7b75f251f34b9e16a40cd9
Aurélien JANVIER <aurel2108@gmail.com>
2015-05-15 12:49:35 +0200

    Adding frm method to Tickets Creating the view to add a ticket

--- app/controllers/Tickets.php
+++ app/controllers/Tickets.php
@@ -25,4 +25,8 @@ class Tickets extends \_DefaultController {
 				})");
 		}
 	}
+
+    public function frm($id=NULL) {
+        $this->loadView("ticket/vAdd");
+    }
 }
---
+++ app/views/ticket/vAdd.php
@@ -0,0 +1,11 @@
+<form method="post" action="tickets/add">
+    <fieldset>
+        <legend>Créer un ticket</legend>
+        <div class="form-group">
+        </div>
+        <div class="form-group">
+            <input type="submit" value="Valider" class="btn btn-default">
+            <a class="btn btn-default" href="<?php echo $config["siteUrl"]?>tickets">Annuler</a>
+        </div>
+    </fieldset>
+</form>
